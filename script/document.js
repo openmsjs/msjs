1d8205443e50f86c08a34067eb758b534a909cef
Adam Wolff <adam@elasticprocess.com>
2010-03-26 08:46:28 -0700

    typo

--- script/document.js
+++ script/document.js
@@ -1,7 +1,7 @@
 /**
     msjs provides a somewhat-faithful DOM representation on the server that
     can be mainpulated before the page loads, accessed through the global variable 
-    "document. It supports most of the regular DOM APIs, enough, at least to
+    "document." It supports most of the regular DOM APIs, enough, at least to
     convince jQuery that it's working with a proper DOM. Beware, though, that once the 
     page is initially rendered, subsequent DOM updates all happen in the browser.
 
