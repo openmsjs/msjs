b697a262f6a1a298953a9eacb1eb91e349720f3d
Adam Wolff <adam@elasticprocess.com>
2010-04-01 13:02:56 -0700

    Remove log statements

--- script/test/pages/regression/msjs-58.js
+++ script/test/pages/regression/msjs-58.js
@@ -3,9 +3,7 @@ var tt = $(<table id="db">
     <tbody/> 
 </table>).appendTo("body"); 
 
-msjs.log("***");
 var tbody = $("#db tbody"); 
-msjs.log(tbody, tbody[0]);
 var node = msjs(function(msj) { 
     $("<tr><td>hello</td></tr>").appendTo("#db tbody"); 
 }); 
