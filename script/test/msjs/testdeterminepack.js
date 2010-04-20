e3de3f0735bba94c3b38af2bc332a1c01eeba9dd
Adam Wolff <adam@elasticprocess.com>
2010-04-19 17:12:03 -0700

    Fix packing test

--- script/test/msjs/testdeterminepack.js
+++ script/test/msjs/testdeterminepack.js
@@ -11,14 +11,18 @@ var referFunctionThatRefersUnpackable = msjs(function(){
 });
 
 
-var circular = msjs();
+function getNode(messenger){
+    return messenger.graph.getNode(messenger.getId());
+}
+
+var circular = getNode(msjs());
 circular.ref = {circular : circular}
 
 var assert = msjs.require("msjs.assert");
 assert("Nodes that refer to singletons shouldn't be packed", 
-        msjs.isPackable(referSingleton) == false);
+        msjs.isPackable(getNode(referSingleton)) == false);
 
 assert("Nodes that refer to functions that refer to unpackable objects shouldn't be packed", 
-       msjs.isPackable(referFunctionThatRefersUnpackable) == false);
+       msjs.isPackable(getNode(referFunctionThatRefersUnpackable)) == false);
 
 assert("Circular reference works", msjs.isPackable(circular) == null);
